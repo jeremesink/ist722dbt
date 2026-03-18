24054a22c6953e40a28ce93b434b1f63fd99de16
Jereme Sienkiewicz <jsienkie@g.syr.edu>
2026-03-18 21:28:58 +0000

    l

--- quickstart/models/FM-FF/stg_ff_account_titles.sql
+++ quickstart/models/FM-FF/stg_ff_account_titles.sql
@@ -2,7 +2,7 @@ select
     AT_ID,
     AT_ACCOUNT_ID,
     AT_TITLE_ID,
-    to_timestamp_ntz(AT_QUEUE_DATE / 1000000) as order_date,
-    to_timestamp_ntz(AT_SHIPPED_DATE / 1000000) as shipped_date,
-    to_timestamp_ntz(AT_RETURNED_DATE / 1000000) as returned_date
+    to_timestamp_ntz(try_to_number(AT_QUEUE_DATE) / 1000000) as order_date,
+    to_timestamp_ntz(try_to_number(AT_SHIPPED_DATE) / 1000000) as shipped_date,
+    to_timestamp_ntz(try_to_number(AT_RETURNED_DATE) / 1000000) as returned_date
 from {{ source ('fudgeflix', 'ff_account_titles') }}
