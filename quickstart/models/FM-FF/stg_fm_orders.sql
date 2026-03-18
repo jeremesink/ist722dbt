d30d0b93d3a559db90541ffa3aade6a7dd21bbe5
Jereme Sienkiewicz <jsienkie@g.syr.edu>
2026-03-18 21:50:19 +0000

    l

--- quickstart/models/FM-FF/stg_fm_orders.sql
+++ quickstart/models/FM-FF/stg_fm_orders.sql
@@ -1,6 +1,6 @@
 select
     order_id,
     customer_id,
-    to_timestamp(order_date/1000000) as order_date,
-    to_timestamp(shipped_date/1000000) as shipped_date
+    to_timestamp_ntz(try_to_number(order_date) / 1000000) as order_date,
+    to_timestamp_ntz(try_to_number(shipped_date) / 1000000) as shipped_date
 from {{ source('fudgemart', 'fm_orders') }}
