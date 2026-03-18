e7bacd9a7cc6d572388acc8ca3b676594d200d80
Jereme Sienkiewicz <jsienkie@g.syr.edu>
2026-03-18 20:15:38 +0000

    f

--- quickstart/models/FM-FF/stg_ff_account_titles.sql
+++ quickstart/models/FM-FF/stg_ff_account_titles.sql
@@ -6,4 +6,4 @@ select
     try_to_timestamp_ntz(at.at_shipped_date / 1e6) as shipped_date,
     try_to_timestamp_ntz(at.at_returned_date / 1e6) as returned_date,
 
-from {{ source('fudgeflix', 'ff_account_titles') }}
+from {{ source('fudgeflix', 'ff_account_titles') }} as at
